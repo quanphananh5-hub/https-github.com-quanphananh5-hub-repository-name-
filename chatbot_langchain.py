5beab8aa61bd0adeb13d32ec77fef33a2fdd1966
quanphananh5-hub <quanphananh5@gmail.com>
2026-07-07 09:15:12 +0700

    Update chatbot_langchain.py

--- chatbot_langchain.py
+++ chatbot_langchain.py
@@ -1,6 +1,6 @@
 # chatbot_langchain.py
 from flask import Flask, request, jsonify
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI   # sửa import
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
